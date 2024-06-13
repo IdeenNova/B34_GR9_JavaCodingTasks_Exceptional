7950af338f5f4b2b92ad4f372da26920486d99a4
olgashyyan.uk@gmail.com <olgashyyan.uk@gmail.com>
2024-06-13 14:13:02 -0400

    Add reverseNegative method to reverseNegativeNumber.java

--- src/codingTasks/olga/week3/reverseNegativeNumber.java
+++ src/codingTasks/olga/week3/reverseNegativeNumber.java
@@ -5,5 +5,24 @@ public class reverseNegativeNumber {
     //ex: given int is ==> -53
     //  the output will be ==> -35
 
+    public static int reverseNegative(int num) {
+        if (num >= 0) {
+            throw new IllegalArgumentException("Input must be a negative number");
+        }
+        // Convert the negative number to a string to easily reverse its digits
+        String numString = Integer.toString(num);
+        // Reverse the string excluding the '-' sign
+        String reversedString = new StringBuilder(numString.substring(1)).reverse().toString();
+        // Append the '-' sign to the reversed string and parse it back to an integer
+        int reversedNum = Integer.parseInt("-" + reversedString);
+        return reversedNum;
+    }
+
+    public static void main(String[] args) {
+        int input = -64;
+        int reversed = reverseNegative(input);
+        System.out.println("Reversed number: " + reversed);
+    }
+
 }
 
