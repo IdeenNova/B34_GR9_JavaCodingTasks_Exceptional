0744c61527b0fe810794c29949e12d849c23361f
IdeenNova <ideen@cydeo.com>
2024-07-03 15:39:26 -0400

    add week5 and 6 tasks

---
+++ src/codingTasks/mentor/week6/FindMaximum.java
@@ -0,0 +1,7 @@
+package codingTasks.mentor.week6;
+
+public class FindMaximum {
+    //Write a method that can find the maximum number from an int Array
+
+
+}
---
+++ src/codingTasks/mentor/week6/sumOfDigits.java
@@ -0,0 +1,11 @@
+package codingTasks.mentor.week6;
+
+public class sumOfDigits {
+
+
+
+
+        //Write a method that can return the sum of the digits in a string
+
+
+}
