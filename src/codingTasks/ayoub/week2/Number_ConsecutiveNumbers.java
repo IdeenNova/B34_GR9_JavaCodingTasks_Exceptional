dfb8bf951bfa3b02a470a4e7c24ea225ebf2c381
aesse <a.esseakh@gmail.com>
2024-07-06 17:08:43 -0400

    java tasks week 1, from 2 classes

--- src/codingTasks/ayoub/week2/Number_ConsecutiveNumbers.java
+++ src/codingTasks/ayoub/week2/Number_ConsecutiveNumbers.java
@@ -2,6 +2,36 @@ package codingTasks.ayoub.week2;
 
 public class Number_ConsecutiveNumbers {
 
+    public static void main(String[] args) {
+        printNumbers(24);
+    }
+        public static void printNumbers(int N) {
+            for (int i = 1; i <= N; i++) {
+                if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("CodilityTestCoders");
+                } else if (i % 2 == 0 && i % 3 == 0) {
+                    System.out.println("CodilityTest");
+                } else if (i % 2 == 0 && i % 5 == 0) {
+                    System.out.println("CodilityCoders");
+                } else if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("TestCoders");
+                } else if (i % 2 == 0) {
+                    System.out.println("Codility");
+                } else if (i % 3 == 0) {
+                    System.out.println("Test");
+                } else if (i % 5 == 0) {
+                    System.out.println("Coders");
+                } else {
+                    System.out.println(i);
+                }
+            }
+        }
+
+
+
+
+
+
 
     /*
     Write a function:
--- src/codingTasks/ayoub/week2/Number_DivisibleBy3_5_15.java
+++ src/codingTasks/ayoub/week2/Number_DivisibleBy3_5_15.java
@@ -2,6 +2,44 @@ package codingTasks.ayoub.week2;
 
 public class Number_DivisibleBy3_5_15 {
 
+    public static void main(String[] args) {
+
+        int n = 100;
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0 && i % 15 == 0) {
+                System.out.print(i + " ");
+            } else if (i % 3 == 0 && i % 5 != 0) {
+                System.out.print(i + " ");
+            } else if (i % 5 == 0 && i % 15 == 0) {
+                System.out.println();
+                System.out.print("Divisible By 5: ");
+                for (; i <= n; i++) {
+                    if (i % 5 == 0 && i % 15 != 0) {
+                        System.out.print(i + " ");
+                    }
+                }
+                break;
+            } else if (i % 15 == 0) {
+                System.out.println();
+                System.out.print("Divisible By 15: ");
+                for (; i <= n; i++) {
+                    if (i % 15 == 0) {
+                        System.out.print(i + " ");
+                    }
+                }
+                break;
+            }
+        }
+        System.out.println();
+        System.out.println("Divisible By 3: ");
+        for (int j = 1; j <= n; j++) {
+            if (j % 3 == 0 && j % 5 != 0 && j % 15 != 0) {
+                System.out.print(j + " ");
+            }
+        }
+    }
+
+
     /*
     Write a program that can print the numbers between 1 ~ N that can be divisible by 3, 5, and 15.
             if the number can be divisible by 3, 5 and 15,
